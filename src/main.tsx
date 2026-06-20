df813d09d85fb095c6e77294f48ceb185c1eb227
varchar1206 <103070120+varchar1206@users.noreply.github.com>
2026-06-20 01:54:20 +0000

    Configure Amplify frontend

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Amplify } from "aws-amplify";
 import App from "./App.tsx";
+import outputs from "../amplify_outputs.json";
 import "./index.css";
+
+Amplify.configure(outputs);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
