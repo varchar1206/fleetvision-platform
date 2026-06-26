9a8fc890c9dd074746264a9ae79f3344e03fb76f
varchar1206 <103070120+varchar1206@users.noreply.github.com>
2026-06-26 19:36:58 +0000

    Remove legacy portal identity layer

--- src/portal/auth/IdentityProvider.ts
+++
@@ -1,6 +0,0 @@
-import type { PortalKey } from "../config/portalDefinitions";
-import type { PortalIdentity } from "./PortalIdentity";
-
-export interface IdentityProvider {
-  getCurrentIdentity(portal: PortalKey): Promise<PortalIdentity>;
-}
